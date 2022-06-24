d5f0452bf166ce4ef94bfce88fc49c1436058a3b
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2022-06-24 15:35:44 -0500

    Primeros ejercicios resueltos

---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+# Ejercicio 4
+
+# Escribí un programa que solicite al usuario ingresar la cantidad de kilómetros recorridos por una motocicleta
+# y la cantidad de litros de combustible que consumió durante ese recorrido. Mostrar el consumo de combustible por kilómetro.
+
+kilometros = int(input("Ingresa los kilómetros recorridos: "))
+litros = int(input("Ingresa los litros consumidos durante el recorrido: "))
+consumoCombustible = kilometros / litros
+print("El consumo por kilómetro es de: ", consumoCombustible)
