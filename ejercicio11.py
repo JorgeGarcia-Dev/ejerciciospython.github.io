6e95b1e6bad855eba7801ce1c73076a45b2668b6
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2022-07-04 11:48:30 -0500

    2 nuevos ejercicios resueltos

---
+++ ejercicio11.py
@@ -0,0 +1,12 @@
+# Escribir un programa que solicite al usuario el ingreso de dos números diferentes
+# y muestre en pantalla al mayor de los dos.
+
+num1 = int(input("Ingresa un número entero: "))
+num2 = int(input("Ingresa un segundo número entero: "))
+
+if num1 > num2:
+    print("El Mayor es: ", num1)
+elif num1 < num2:
+    print("El Mayor es: ", num2)
+else:
+    print("Ingresa números enteros")
---
+++ ejercicio12.py
@@ -0,0 +1,12 @@
+# Escribir un programa para solicitar al usuario tres números y mostrar en pantalla al menor de los tres.
+
+num1 = int(input("Ingresa un número entero: "))
+num2 = int(input("Ingresa un segundo número entero: "))
+num3 = int(input("Ingresa un tercer número entero: "))
+
+if num1 < num2 and num1 < num3:
+    print("El número menor es: " + str(num1))
+elif num2 < num1 and num2 < num3:
+    print("El número menor es: " + str(num2))
+else:
+    print("El número menor es: " + str(num3))
