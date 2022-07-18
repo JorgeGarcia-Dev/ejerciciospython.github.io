cb6bc37205ebc2e725fc2e730a397cb3487065b3
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2022-07-18 18:49:48 -0500

    Nuevo ejercicio resuelto y corregido

--- ejercicio17.py
+++ ejercicio17.py
@@ -1,9 +1,8 @@
 # Escribir un programa que le solicite al usuario ingresar una fecha formada por 8 números,
 # donde los primeros dos representan el día, los siguientes dos el mes y los últimos cuatro el año (DDMMAAAA).
-# Este dato debe guardarse en una variable con tipo int (número entero).
 # Finalmente, mostrar al usuario la fecha con el formato DD / MM / AAAA.
 
-numeros = int(input("ingresa un número de 8 caractéres: "))
+numeros = (input("ingresa un número de 8 caractéres: "))
 dia = numeros[:2]
 mes = numeros[2:4]
 año = numeros[4:]
